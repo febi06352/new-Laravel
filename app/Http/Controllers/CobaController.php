79abe5d175c1f0715de41a2e2a288f24d5737a21
febi06352 <riskifebi96@gmail.com>
2018-12-23 12:07:33 +0700

    Add files via upload

---
+++ app/Http/Controllers/CobaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Student;
+use Illuminate\Support\Facades\Hash;
+
+class CobaController extends Controller
+{
+    public function tes()
+    {
+        $febi = new Student();
+        $febi->name = 'tes';
+        $febi->username = 'user1';
+        $febi->password = Hash::make('password');
+        $febi->save();
+    }
+
+    public function index()
+    {
+        dd(Student::all()->toArray());
+    }
+
+}
